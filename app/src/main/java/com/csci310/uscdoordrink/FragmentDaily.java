25d8f6edea9e95b5efd2da10990ccb86ad8fbf90
Jinghao Wang <jinghao@usc.edu>
2022-03-23 23:45:06 -0700

    Analysis Page UI
    Tabbed layout for chart analysis of spendings

---
+++ app/src/main/java/com/csci310/uscdoordrink/FragmentDaily.java
@@ -0,0 +1,17 @@
+package com.csci310.uscdoordrink;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentDaily extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragement_daily_layout,container, false);
+    }
+}
---
+++ app/src/main/java/com/csci310/uscdoordrink/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,61 @@
+package com.csci310.uscdoordrink.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.csci310.uscdoordrink.FragmentDaily;
+import com.csci310.uscdoordrink.R;
+import com.csci310.uscdoordrink.FragmentMonthly;
+import com.csci310.uscdoordrink.FragmentWeekly;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_daily, R.string.tab_weekly,R.string.tab_monthly};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        Fragment fragment = null;
+        switch(position){
+            case 0:
+                fragment = new FragmentDaily();
+                break;
+            case 1:
+                fragment = new FragmentWeekly();
+                break;
+            case 2:
+                fragment = new FragmentMonthly();
+                break;
+        }
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 3;
+    }
+}
